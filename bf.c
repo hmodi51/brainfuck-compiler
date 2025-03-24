6e90d87b921679b226d1e524e0dd83fb5979bd68
hmodi51 <modiharshit01@gmail.com>
2025-03-24 08:06:44 -0500

    implemented bf to assembly

--- bf.c
+++ bf.c
@@ -3,52 +3,82 @@
 #include <stdlib.h>
 
 
-char *code;
-int codeSize = 10000;
+char *code;                             // Stores generated assembly code
+int codeSize = 20000;                   // Max size of assembly code
+int *loopIds;                           // Stack to manage nested loops
+int loopCount = 0;                       //  Counter for unique loop labels
+int totalLoopCap = 100;                   // Initial capacity for loop stack
+int currId = 0;                           // Current loop ID
+int lastLoopIndex = 0;                     // Index to last loop
 
 
+
+// Function to convert Brainfuck code to x86_64 assembly
 void bftoc(char* buf){
     int length = strlen(buf);
 
     for (int i =0 ; i < length ; i++){
-        if(buf[i] == '>'){
-            char *text = "inc %rsi";
+        if(buf[i] == '>'){                 // Move pointer to the right
+            char *text = "incq %rsi\n";                  
             snprintf(code + strlen(code) , codeSize - strlen(code) , text);
         }
-        else if(buf[i] == '<'){
-            char *text = "dec %rsi";
+        else if(buf[i] == '<'){            // Move pointer to the left
+            char *text = "decq %rsi\n";
             snprintf(code + strlen(code) , codeSize - strlen(code) , text);
         }
 
-        else if(buf[i] == '+'){
-            char *text = "inc (%rsi)";
+        else if(buf[i] == '+'){            // Increment byte at pointer
+            char *text = "incb (%rsi)\n";
             snprintf(code + strlen(code) , codeSize- strlen(code) , text);
         }
 
-        else if(buf[i] == '-'){
-            char *text = "dec (%rsi)";
+        else if(buf[i] == '-'){             // Decrement byte at pointer
+            char *text = "decb (%rsi)\n";
             snprintf(code + strlen(code) , codeSize - strlen(code) , text);
         }
 
-        else if(buf[i] == '['){
-            char* text = "loop:  "
-                         "  cmp $0 , (%rsi)"
-                         "  jz  loop_end"
-                         "  jne loop";
-            snprintf(code + strlen(code) , codeSize - strlen(code) , text);
+        else if(buf[i] == '['){             // Start loop
+            
+
+            // if loopcount exceeds total loop capacity then dynamically reallocate more loop space
+            if(loopCount == totalLoopCap){
+                totalLoopCap *=2;
+                loopIds = realloc(loopIds , totalLoopCap * sizeof(int));
+            }
+
+            // add loop to the array
+            loopIds[lastLoopIndex++] = loopCount;
+            snprintf(code + strlen(code) , codeSize - strlen(code) , "loop%d:\n"
+                                                                     "  cmpb $0 , (%rsi)\n"
+                                                                      "  jz  loop_end%d\n" ,
+                                                                       loopCount , loopCount);
+            loopCount++;
         }
+
+
 
         else if(buf[i] == ']'){
-            char*text = "loop_end: ";
+            currId = loopIds[--lastLoopIndex];    // Pop loop ID from array
+            snprintf(code + strlen(code) , codeSize - strlen(code) , "jmp loop%d\nloop_end%d:\n" , currId , currId);
+        }
+
+        else if(buf[i] == '.'){    // Output character to stdout
+           char* text = "mov $1 , %rax\n"
+                        "mov $1 , %rdi\n"
+                        "mov $1 , %rdx\n"
+                        "syscall\n";
+
             snprintf(code + strlen(code) , codeSize - strlen(code) , text);
         }
 
-        else if(buf[i] == '.'){
-            snprintf(code + strlen(code) , codeSize - strlen(code) , "putchar(*ptr);\n");
-        }
+        else if(buf[i] == ','){    // input character from stdinc
+            
+            char* text = "mov $0 , %rax\n"
+                         "mov $0 , %rdi\n"
+                         "mov $1 , %rdx\n"
+                         "syscall\n";
 
-        else if(buf[i] == ','){
-            snprintf(code + strlen(code) , codeSize - strlen(code) ,"*ptr = getchar();\n");
+            snprintf(code + strlen(code) , codeSize - strlen(code) , text);
 
         }
     }
@@ -59,46 +89,55 @@ void bftoc(char* buf){
 
 int main(int argc , char* argv[]) {
 
+    // Check for input file
     if(argc < 2){
         return 1;
     }
 
-    char text[1000] = "; bf to asm code"
-                    ".section     .text      # declare the .text section"
-                    "globl       _start    # declare global section"
-                    " _start:"
-                    "   subq $30000 , %rsp    # allocate 30000 bytes on stack "
-                    "   movq %rsp , %rcx       # have rcx point to same place as rsp"     
-                    "   movq $30000 , %rbx     # copy 30000 into rbx register as it will serve as count"
-                    "   zeroed:                 # make label zeroed which will zero all 30000 bytes"              
-                    "    movb  $0 , (%rcx)       # copy 0 into current byte"     
-                    "    inc %rcx                # move rcx to next byte"
-                    "    dec %rbx                # decrement rbx by 1 "
-                    "    cmp $0 , %rbx           # compare rbx and 0"
-                    "    jne zeroed              # if rbx still not zero then go loop again"
+    // initialize memory
+    char text[1000] = "# bf to asm code\n"
+                    ".section     .text      # declare the .text section\n"
+                    ".globl       _start    # declare global section\n"
+                    " _start:\n"
+                    "   subq $30000 , %rsp    # allocate 30000 bytes on stack \n"
+                    "   movq %rsp , %rcx       # have rcx point to same place as rsp\n"     
+                    "   movq $30000 , %rbx     # copy 30000 into rbx register as it will serve as count\n"
+                    "   zeroed:                 # make label zeroed which will zero all 30000 bytes\n"              
+                    "    movb  $0 , (%rcx)       # copy 0 into current byte\n"     
+                    "    incq %rcx                # move rcx to next byte\n"
+                    "    decq %rbx                # decrement rbx by 1 \n"
+                    "    cmpq $0 , %rbx           # compare rbx and 0\n"
+                    "    jne zeroed              # if rbx still not zero then go loop again\n"
                     
-                    "#zeroed all 30000 bytes till here"
-                    "   movq %rsp , %rsi           #copy rsp to rsi as char* ptr = array";
+                    "#zeroed all 30000 bytes till here\n"
+                    "   movq %rsp , %rsi           #copy rsp to rsi as char* ptr = array\n";
 
-    code = malloc(10000); 
-    code[0] = '\0';
+    // Allocate memory for code
+    code = malloc(20000); 
+    code[0] = '\0';    // input null character to make it empty string
     snprintf(code + strlen(code) , codeSize - strlen(code) , text);
 
+    // Allocate memory for loop array
+    loopIds = malloc(totalLoopCap * sizeof(int));
+
+
+    // Open Brainfuck source file
     FILE *fptr;
-
-
-
     char* filename = argv[1];
     fptr = fopen(filename , "r");
     
-    char buf[10000];
+    char buf[20000];
 
+    // Read file contents into buffer
     while (fgets(buf + strlen(buf), sizeof(buf) - strlen(buf), fptr) != NULL);
 
-    bftoc(buf);
+    bftoc(buf);  // call bftoc func which will convert bf code to assembly
 
-    snprintf(code + strlen(code) , sizeof(code) - strlen(code) , "return 0; \n }\n");
-
+    // Append exit syscall
+    snprintf(code + strlen(code) , sizeof(code) - strlen(code) , "movq $60 , %rax\n"
+                                                                  "xor %rdi , %rdi\n"
+                                                                   "syscall\n");
+    // Write assembly to file
     fclose(fptr);
     fptr = fopen("output.s" , "w");
     fprintf(fptr , code);
