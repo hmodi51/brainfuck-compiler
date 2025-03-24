69006d0936f000f97ebf04214461052c52070d1c
hmodi51 <modiharshit01@gmail.com>
2025-03-24 04:18:58 -0500

    first try on converting to assembly

--- bf.c
+++ bf.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 
 
-char code[10000];
+char *code;
+int codeSize = 10000;
 
 
 void bftoc(char* buf){
@@ -11,34 +12,43 @@ void bftoc(char* buf){
 
     for (int i =0 ; i < length ; i++){
         if(buf[i] == '>'){
-            snprintf(code + strlen(code) , sizeof(code) - strlen(code) , "ptr++;\n");
+            char *text = "inc %rsi";
+            snprintf(code + strlen(code) , codeSize - strlen(code) , text);
         }
         else if(buf[i] == '<'){
-            snprintf(code + strlen(code) , sizeof(code) - strlen(code) , "ptr--;\n");
+            char *text = "dec %rsi";
+            snprintf(code + strlen(code) , codeSize - strlen(code) , text);
         }
 
         else if(buf[i] == '+'){
-            snprintf(code + strlen(code) , sizeof(code) - strlen(code) , "(*ptr)++;\n");
+            char *text = "inc (%rsi)";
+            snprintf(code + strlen(code) , codeSize- strlen(code) , text);
         }
 
         else if(buf[i] == '-'){
-            snprintf(code + strlen(code) , sizeof(code) - strlen(code) , "(*ptr)--;\n");
+            char *text = "dec (%rsi)";
+            snprintf(code + strlen(code) , codeSize - strlen(code) , text);
         }
 
         else if(buf[i] == '['){
-            snprintf(code + strlen(code) , sizeof(code) - strlen(code) , "while (*ptr) {\n");
+            char* text = "loop:  "
+                         "  cmp $0 , (%rsi)"
+                         "  jz  loop_end"
+                         "  jne loop";
+            snprintf(code + strlen(code) , codeSize - strlen(code) , text);
         }
 
         else if(buf[i] == ']'){
-            snprintf(code + strlen(code) , sizeof(code) - strlen(code) , "    }\n");
+            char*text = "loop_end: ";
+            snprintf(code + strlen(code) , codeSize - strlen(code) , text);
         }
 
         else if(buf[i] == '.'){
-            snprintf(code + strlen(code) , sizeof(code) - strlen(code) , "putchar(*ptr);\n");
+            snprintf(code + strlen(code) , codeSize - strlen(code) , "putchar(*ptr);\n");
         }
 
         else if(buf[i] == ','){
-            snprintf(code + strlen(code) , sizeof(code) - strlen(code) ,"*ptr = getchar();\n");
+            snprintf(code + strlen(code) , codeSize - strlen(code) ,"*ptr = getchar();\n");
 
         }
     }
@@ -52,7 +62,27 @@ int main(int argc , char* argv[]) {
     if(argc < 2){
         return 1;
     }
-    snprintf(code + strlen(code) , sizeof(code) - strlen(code) , "#include <stdio.h>\n int main(){ \n char array[30000] = {0}; \n char *ptr = array;\n");
+
+    char text[1000] = "; bf to asm code"
+                    ".section     .text      # declare the .text section"
+                    "globl       _start    # declare global section"
+                    " _start:"
+                    "   subq $30000 , %rsp    # allocate 30000 bytes on stack "
+                    "   movq %rsp , %rcx       # have rcx point to same place as rsp"     
+                    "   movq $30000 , %rbx     # copy 30000 into rbx register as it will serve as count"
+                    "   zeroed:                 # make label zeroed which will zero all 30000 bytes"              
+                    "    movb  $0 , (%rcx)       # copy 0 into current byte"     
+                    "    inc %rcx                # move rcx to next byte"
+                    "    dec %rbx                # decrement rbx by 1 "
+                    "    cmp $0 , %rbx           # compare rbx and 0"
+                    "    jne zeroed              # if rbx still not zero then go loop again"
+                    
+                    "#zeroed all 30000 bytes till here"
+                    "   movq %rsp , %rsi           #copy rsp to rsi as char* ptr = array";
+
+    code = malloc(10000); 
+    code[0] = '\0';
+    snprintf(code + strlen(code) , codeSize - strlen(code) , text);
 
     FILE *fptr;
 
@@ -70,7 +100,7 @@ int main(int argc , char* argv[]) {
     snprintf(code + strlen(code) , sizeof(code) - strlen(code) , "return 0; \n }\n");
 
     fclose(fptr);
-    fptr = fopen("output.c" , "w");
+    fptr = fopen("output.s" , "w");
     fprintf(fptr , code);
     fclose(fptr);
 
